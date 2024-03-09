1d7d95f71f34f0d66e883d52515fb56298e67bdc
Rishabh Singh <rishabh797604@gmail.com>
2024-03-09 21:51:35 +0530

    Delete src directory

--- src/components/ListItems.tsx
+++
@@ -1,9 +0,0 @@
-interface ListItemsProps {
-  image: string;
-  name: string;
-  href: string;
-}
-
-export default function ListItems({ image, name, href }: ListItemsProps) {
-  return <button></button>;
-}
--- src/pages/Loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center h-screen w-full ">
-      <div className="h-40 w-40 rounded-full border-t-8 border-sky-400 animate-spin"></div>
-    </div>
-  );
-}
